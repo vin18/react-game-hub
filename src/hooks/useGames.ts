11bd38daa390f6dffd877eb9baea396a12322490
Vinit Raut <vinit.coder@gmail.com>
2023-04-02 21:21:31 +0530

    Implement searching..

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from '../App';
 import useData from './useData';
-import { Genre } from './useGenres';
 import { Platform } from './usePlatforms';
 
 export interface Game {
@@ -19,6 +18,7 @@ const useGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
