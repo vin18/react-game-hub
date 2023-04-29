e462637fc8e6ac694dc81d91764f2068e9f89a74
Vinit Raut <vinit.coder@gmail.com>
2023-04-29 16:46:26 +0530

    Refactor: Entity interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,14 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import ms from 'ms';
 import platforms from '../data/platforms';
 import APIClient from '../services/api-client';
+import { Platform } from '../entities/Platform';
 
 const apiClient = new APIClient<Platform>('/platforms/list/parents');
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
 // const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
 const usePlatforms = () =>
   useQuery({
